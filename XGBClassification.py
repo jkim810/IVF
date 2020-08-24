d71e75f4b23c5ff97213e3c5af288e2e610bbc42
jkim810 <jkim0810@gmail.com>
2020-08-24 17:05:57 -0400

    XGBoost updates

--- XGBClassification.py
+++ XGBClassification.py
@@ -16,7 +16,7 @@ import pandas as pd
 import numpy as np
 import xgboost as xgb
 
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 import matplotlib.pyplot as plt
 
 import seaborn as sns
@@ -27,6 +27,7 @@ import sys
 import copy
 import tqdm
 
+CLASSIFICATION_MODE = 'binary' # 'binary' or 'm-ary'
 
 SEED       = 42
 
@@ -37,47 +38,77 @@ TEST_SIZE  = 0.2
 BATCH_SIZE = 32
 EPOCHS     = 100
 
-MASTER_FILE = 'data.csv'
+MASTER_FILE = 'meta_numeric.csv'
 
 # Load ImageNet class names
 labels_map = {'EUP':0,
-              'ANU':1,
-              'CxA':2,
-              'MUT':3}
+			  'ANU':1,
+			  'CxA':2}
 
+binary_map = {'EUP':0,
+			  'CxA':1}
+
+
+param = {'max_depth': 4,
+		 'learning_rate': 1e-2,
+		 'objective':'multi:softmax',
+		 'random_state':SEED,
+		 'eval_metric':['mlogloss', 'merror'],
+		 'n_estimators':1000,
+		 'random_state':SEED}
+	
+binary_param = {'max_depth': 4,
+			   'learning_rate': 1e-3,
+			   'objective':'binary:logistic',
+			   'random_state':SEED,
+			   'eval_metric':['logloss', 'error'],
+			   'n_estimators':1000,
+			   'random_state':SEED}
 # Implementation of custom dataset class to use dataloader
 
 if __name__ == '__main__':
-       
-    # Read data
-    df = pd.read_csv(MASTER_FILE)
-    df['LABEL'] = df['PGD_RESULT'].replace(labels_map)
-    df = df.drop(columns = ['FILENAME', 'PGD_RESULT','ab_or_norm', 'GRADE', 'Expansion', 'ICM', 'TE', 'BS', 'BMS'])
-    
-    # Split dataset
-    train_df, valid_df, test_df = np.split(df.sample(frac=1), [int(VALID_SIZE*len(df)), int(TEST_SIZE*len(df))])
-    
-    X_train = train_df.drop(columns='LABEL')
-    y_train = train_df['LABEL']
-    X_valid = valid_df.drop(columns='LABEL')
-    y_valid = valid_df['LABEL']
-    X_test = test_df.drop(columns='LABEL')
-    y_test = test_df['LABEL']
-    '''
-    dtrain = xgb.DMatrix(X_train, label = y_train)
-    dvalid = xgb.DMatrix(X_valid, label = y_valid)
-    dtest = xgb.DMatrix(X_test, label = y_test)
-    '''
-    param = {'max_depth': 6,
-             'learning_rate': 5e-3,
-             'objective':'multi:softmax',
-             'random_state':SEED,
-             'eval_metric':'mlogloss',
-             'n_estimators':1000}
-    
-    model = xgb.XGBClassifier(**param)
-    model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], early_stopping_rounds=20)
-    
-    pred_test = model.predict(X_test)
-    print(np.mean(pred_test == y_test))
-    print(model.feature_importances_)
+	if CLASSIFICATION_MODE == 'binary':
+	  labels_map = binary_map
+	  param = binary_param
+
+	np.random.seed(SEED)
+
+	# Read data
+	df = pd.read_csv(MASTER_FILE, index_col=0)
+	df = df[df['PGD_RESULT'].isin(labels_map.keys())]
+	df['LABEL'] = df['PGD_RESULT'].replace(labels_map)
+	
+	df = df.select_dtypes(exclude=['object'])
+	df = df.drop(columns = ['BS', 'BMS', 'ICM', 'TE', 'Expansion'])
+	
+	# Split dataset
+	train_df, valid_df, test_df = np.split(df.sample(frac=1), [int(TRAIN_SIZE*len(df)), int((TRAIN_SIZE+VALID_SIZE)*len(df))])
+	print(len(train_df), len(valid_df), len(test_df))
+	
+	X_train = train_df.drop(columns='LABEL')
+	y_train = train_df['LABEL']
+	X_valid = valid_df.drop(columns='LABEL')
+	y_valid = valid_df['LABEL']
+	X_test = test_df.drop(columns='LABEL')
+	y_test = test_df['LABEL']
+
+	'''
+	dtrain = xgb.DMatrix(X_train, label = y_train)
+	dvalid = xgb.DMatrix(X_valid, label = y_valid)
+	dtest = xgb.DMatrix(X_test, label = y_test)
+	'''
+	
+	model = xgb.XGBClassifier(**param)
+	model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], early_stopping_rounds=20)
+	
+	y_pred = model.predict(X_test)
+	
+	print('Accuracy: {:.2f}\n'.format(accuracy_score(y_test, y_pred) * 100))
+	print(pd.DataFrame(confusion_matrix(y_test,y_pred),index = labels_map.keys(), columns = labels_map.keys()))
+	print(classification_report(y_test, y_pred, target_names = labels_map.keys()))
+	
+	#print(pd.DataFrame({'feature' : df.columns, 'Importance': model.feature_importances_}))
+	from xgboost import plot_importance
+	
+	plot_importance(model)
+	plt.savefig('featimp.png')
